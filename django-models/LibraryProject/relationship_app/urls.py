ae1a456bbb1dd6570b048d55140f8e84aa4e92d6
ugochukwu mbagwu <ugochukwumbagwu@gmail.com>
2026-01-25 14:34:11 -0900

    relationship

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,25 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from .views import list_books
+from .views import LibrayDetailView, LoginView, LogoutView, SignUpView, logout_confirm
+#LibraryDetailView
 
+app_name='rel_app'
+# views.register
+# add_book/
+# edit_book/
+# path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html), name='logout')
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name="relationship_app/login.html"), name='login'),
-    path('logout/', LogoutView.as_view(template_name="relationship_app/logout.html"), name='logout'),
+    path('fbv/', list_books, name='fbv_details'),
+    path('cbv/<int:pk>/', LibrayDetailView.as_view(), name='cbv_details'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', SignUpView.as_view(), name='register'),
+    path('logout_confirm/', logout_confirm, name='logout_confirm'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
