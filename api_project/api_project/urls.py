d80eeba0d3940f3cdd98fdff5e123ff0bd1719de
ugochukwu mbagwu <ugochukwumbagwu@gmail.com>
2026-02-10 20:22:49 -0900

    api

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework import DefaultRouters
 from .views import BookList, BookViewSet
 
 router = routers.DefaultRouter()
-router.register(r'book_all', BookViewSet, basename='book_all')
+router.register(r'book_all', view.BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
