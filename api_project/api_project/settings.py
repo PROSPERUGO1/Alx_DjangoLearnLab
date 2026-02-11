15756d18f2bea5234c4ecf6076379e462e049397
ugochukwu mbagwu <ugochukwumbagwu@gmail.com>
2026-02-10 21:07:52 -0900

    api

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api.apps.ApiConfig',
-    'restframwwork.authtoken',
+    'rest_framwwork.authtoken',
 ]
 
 REST_FRAMEWORK = {
