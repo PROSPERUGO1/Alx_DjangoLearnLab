2703e0797086ef675e3f81d7a8b8dc9f40c76216
ugochukwu mbagwu <ugochukwumbagwu@gmail.com>
2026-03-04 16:12:33 -0900

    socials

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('', include(router.urls)),
     # Feed endpoint
     path('feed/', views.FeedView.as_view(), name='feed'),
+     path('<int:pk>/like/', views.PostLikeView.as_view(), name='post-like'),
+    path('<int:pk>/unlike/', views.PostUnlikeView.as_view(), name='post-unlike'),
+
 ]
